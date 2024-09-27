1c0d85970f0b77064298b99fa3382cd10c5b7c42
nitram26 <carlosmartin1993@gmail.com>
2024-09-27 10:35:22 -0500

    2409271035

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,62 +1,31 @@
 <template>
-  <h1>VUE3</h1>
-    <div class="uno">
-    
-      <button @click="handleLayout(ListLayout)">LISTA</button>
-      <button  @click="handleLayout(CardLayout)">TARJETA</button>
-      <button  @click="handleLayout(TableLayout)">TABLA</button>
+    <div>
+      <Transition>
+        <h1>ANIMACIONES en VUE3 COMPOSITION</h1>
+      </Transition>
     </div>
-    <component :is="layout" :content="users"/>
+    <v-btn>Mostrar</v-btn>
 </template>
 <!-- ************************************* -->
 <script setup>
-import { ref, defineAsyncComponent } from 'vue';
 
-
-const ListLayout = defineAsyncComponent(()=> import('@/layouts/ListLayout.vue'))
-const TableLayout= defineAsyncComponent(() => import('@/layouts/tableLayout.vue'))
-const CardLayout= defineAsyncComponent(()=> import('@/layouts/CardLayout.vue'))
-
-const layout = ref(ListLayout)
-const handleLayout = (cmp) => layout.value = cmp
-
-
-const users = ref([
-  {
-    name:'Carlos',
-    age: 65,
-    pocition: 'desarrollador'
-  },
-  {
-    name:'Martin',
-    age: 11,
-    pocition: 'back'
-  },
-  {
-    name:'Nestor',
-    age: 22,
-    pocition: 'front'
-  },
-  {
-    name:'Jose',
-    age: 33,
-    pocition: 'bd'
-  },
-  {
-    name:'Pedro',
-    age: 44,
-    pocition: 'analista'
-  },
-])
 </script>
 <!-- ************************************* -->
 <style scoped>
-.uno{
-  display: flex;
+.v-enter-from, .v-leave-to{
+  opacity: 0;
 }
-.uno button{
-  background-color: brown;
-  color: aliceblue;
-  margin: 1em;
+
+.v-enter-active, .v-leave-active{
+  transition: opacity 0.5s ease ;
 }
-</style>
+
+.v-enter-to{
+
+}
+
+.v-leave-from{
+
+}
+
+</style> 
